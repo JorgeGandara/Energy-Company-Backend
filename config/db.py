810cd4ae15069939d55c4c976c5b4493540b1620
Jorge Gandara <gandaraj@utb.edu.co>
2023-11-16 16:51:40 -0500

    Database Conection

--- config/db.py
+++ config/db.py
@@ -1,6 +1,13 @@
+import os
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://energycompanydb:123456@35.188.4.223:3306/energy_company")
+db_username = os.getenv('DB_USERNAME', 'energycompanydb')
+db_password = os.getenv('DB_PASSWORD', 123456)
+db_host = os.getenv('DB_HOST', "35.188.4.223")
+db_port = int(os.getenv('PORT', 3306))
+db_name = os.getenv('DB_DBNAME', 'energy_company')
+
+engine = create_engine(f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}")
 
 meta_data = MetaData()
 meta_data.bind = engine
