318a11b18cede8d19f9c79e9fb616ebf3bb3f48b
Jorge Gandara <gandaraj@utb.edu.co>
2023-11-16 14:39:19 -0500

    Connection to database

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:123456@localhost:3306/energy_company")
+engine = create_engine("mysql+pymysql://energycompanydb:123456@35.188.4.223:3306/energy_company")
 
 meta_data = MetaData()
 meta_data.bind = engine
